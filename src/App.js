5f2c1d5905275dd97d1f837259f411a3be9b6c5c
aymen740 <aymenhfaidh0@gmail.com>
2026-01-02 13:03:20 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <h1>Welcome to my app</h1>
+      <button>
+      I'm a button
+    </button>
+
+    </div>
+  );
+  
+} 
+
+export default App(); 
+
+ 
