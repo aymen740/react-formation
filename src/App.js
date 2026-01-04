1d588b316cbe6a8229a9688e22a39e4706824137
aymen740 <aymenhfaidh0@gmail.com>
2026-01-04 14:58:47 -0800

    hook usestate

--- src/App.js
+++ src/App.js
@@ -1,74 +1,23 @@
 
 import React, { useState } from "react";
-import { Input } from "./components/forms/input";
-const products =[
-  { category: "fruits", price: "$1", stocked: true, name: "apple"},
-   { category: "fruits", price: "$1", stocked: true, name: "dragonfruit"},
-    { category: "fruits", price: "$1", stocked: false, name: "passion fruit"},
-     { category: "vegetables", price: "$4", stocked: true, name: "spinach"},
-      { category: "fruits", price: "$1", stocked: false, name: "pumpkin"},
-       { category: "vegetables", price: "$2", stocked: true, name: "peas"}
 
-
-  
-]
 function App() {
-  const [showStockedOnly, setshowStockedOnly] = useState(false)
-  const [search, setsearch] = useState('')
-  const visibleProducts = products.filter(product => {
-    if (showStockedOnly && !product.stocked) {
-      return false
-    }
-    if (search && !product.name.includes(search)) {
-      return false
-    }
-    return true 
-  } )
+  const [person, setperson] = useState({
+  firstName: 'john',
+  lastName: 'Doe',
+  age: 18
+})
 
-return <div className="container my-3">
-  <SearchBar
-  search={search}
-  onsearchChange={setsearch}
-   showStockedOnly={showStockedOnly} onStockedOnlyChange={setshowStockedOnly} />
-<ProductTable products={visibleProducts} />
-</div>
+const [count, setCount] = useState(0)
+const incrementAge = () => {
+  setperson({...person, age: person.age + 1})
 }
-function SearchBar ({showStockedOnly, onStockedOnlyChange, search, onsearchChange}) {
-  return <div>
-    <div className="mb-3">
-      <Input value={search} onChange={onsearchChange} placeholder="Rechercher..."/>
-      <checkbox id="stocked" checked={showStockedOnly} onChange={onStockedOnlyChange} label="N'afficher que les produits 
-      en stock"/>
-    </div>
-  </div>
+ return <>
+ <p>Age de {person.firstName} : {person.age}</p>
+
+  <button onClick={incrementAge}>Gagner une année</button>
+  </>
 }
-
-function ProductTable ({products}) {
-  const rows = []
-  let lastCategory = null
-
-  for (let product of products) {
-    if (product.category !== lastCategory) {
-      rows.push(<productCategoryRow key={product.category} name={product.category} />)
-    }
-    lastCategory = product.category
-    rows.push(<productRow product={product} key={product.name} />)
-  }
-  return <table className="table">
-    <thead>
-      <tr>
-        <th>Nom</th>
-        <th>Prix</th>
-      </tr>
-    </thead>
-    <tbody>
-     {rows}
-    </tbody>
-  </table>
-}
-
-
-
 
 
 export default App()
