38a8427ffa51b185089b5a44d463568571345e9c
aymen740 <aymenhfaidh0@gmail.com>
2026-01-08 02:22:58 -0800

    D

--- src/App.js
+++ src/App.js
@@ -1,98 +1,27 @@
 
-import React from 'react';
-
-
-import { AnimatePresence, motion } from 'framer-motion';
-import { useToggle } from './hooks/useToggle.js'; 
-
-const boxVariants = {
-  visible: { x: 0, rotate: 0, opacity: 1 },
-  hidden: { x: 100, rotate: 45, opacity: 0 },
-};
+import { useState } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
 
 
 
 function App() {
- 
+
+  const [duration, setduration] = useState(5)
+  const [secondsleft, setsecondleft] = useState(duration)
+return <div className="vstack gap-2">
+<input 
+value={duration}
+onChange={setduration}
+placeholder="Timer..."
+/>
+<p>
+  décompte : 
+
+</p>
+</div>
+}
     
 
-  const [open, toggle] = useToggle(true);
-  const items = open ? [1,2,3,4,5] : [3,2,5,1,4];
-
-  return (
-    <div className="container my-4 vstack gap-2">
-      <AnimatePresence mode="popLayout">
-      {open && <motionBox
-    variants={boxVariants}
-    animate="visible"
-    initial="hidden"
-    exit="hidden"
-    >1</motionBox>}  
-    
-      </AnimatePresence>
-      <motion.div
-     
-      >
-        {items.map((item) => (
-          <motionBox layout key={item}>{item}</motionBox>
-        ))}
-        
-      </motion.div>
-      {open ? <Page1/> : <Page2/>}
-      <div>
-        <button onClick={toggle}>Afficher / Masquer</button>
-      </div>
-    </div>
-  );
-}
-
-
-
-
-
-function Page1() {
-  return (
-    <div>
-      <p>
-        lorem ipsum dolr sit amterf djdjsf ddoppalsde laalfghjd afmdbgkl elit ab vbgehu buedvbvburbuvvbuv vzrgzgyv  
-        acdqquitino dhdddjd odio sapiente unde velit dndsvnisvsvn dvsvdb sgrsbuv bfuvvbkufv fbudb gebz sdvsgiuzgb  s fvbv
-        vsinzvgni finvbndbnbi fnibdbnib fvbnibdfbndib fnbdnbfdsvfbb fnn vbdvvkfu fbukfvdvfg v bsdufd dubb sdbvuzbdvsz bgvbkjz 
-      </p>
-       <motion.img
-      layoutId="image"
-      src="https://picsum.photos/id/237/300/200"
-      width={300}
-      height={200}
-      alt=""
-      />
-       <p>
-        lorem ipsum dolr sit amterf djdjsf ddoppalsde laalfghjd afmdbgkl elit ab vbgehu buedvbvburbuvvbuv vzrgzgyv  
-        acdqquitino dhdddjd odio sapiente unde velit dndsvnisvsvn dvsvdb sgrsbuv bfuvvbkufv fbudb gebz sdvsgiuzgb  s fvbv
-        vsinzvgni finvbndbnbi fnibdbnib fvbnibdfbndib fnbdnbfdsvfbb fnn vbdvvkfu fbukfvdvfg v bsdufd dubb sdbvuzbdvsz bgvbkjz 
-      </p>
-
-    </div>
-  );
-}
-
-function Page2() {
-  return (
-    <div>
-      <motion.img
-      layoutId="image"
-      src="https://picsum.photos/id/238/300/200"
-      width={300}
-      height={200}
-      alt=""
-      />
-      <p>
-          lorem ipsum dolr sit amterf djdjsf ddoppalsde laalfghjd afmdbgkl elit ab vbgehu buedvbvburbuvvbuv vzrgzgyv  
-        acdqquitino dhdddjd odio sapiente unde velit dndsvnisvsvn dvsvdb sgrsbuv bfuvvbkufv fbudb gebz sdvsgiuzgb  s fvbv
-        vsinzvgni finvbndbnbi fnibdbnib fvbnibdfbndib fnbdnbfdsvfbb fnn vbdvvkfu fbukfvdvfg v bsdufd dubb sdbvuzbdvsz bgvbkjz
-      </p>
-    </div>
-  )
-}
-
-export default App ()
+  
+export default App 
--- src/hooks/useToggle.js
+++ src/hooks/useToggle.js
@@ -7,7 +7,8 @@
 import  { useState } from "react";
 
  export function  useToggle (initial = false) {
-  const [state, setState] = useState(initial)
+  const [state, setState] = useState(true);
+  
   const toggle = () => setState(v => !v)
   return [state, toggle]
 }
